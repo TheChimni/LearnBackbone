ee3aeb14b950bcc12ffe67fdd198edd241fe0341
Rieethaa <rieethaa@gmail.com>
2011-11-03 01:12:41 +0000

    experimenting with the RecipeView

--- public/recipes.js
+++ public/recipes.js
@@ -1,4 +1,15 @@
 window.Recipe = Backbone.Model.extend({
 	
+});
+
+window.RecipeView = Backbone.View.extend({
+	initialize: function(){
+		this.template = _.template($('#recipeTemplate').html());
+	},
 	
+	render: function(){
+		var renderedContent = this.template(this.model.toJSON());
+		$(this.el).html(renderedContent);
+		return this;
+	}
 });
