c30631a6b9119540ef6aa6309189da58f80dcdde
Rieethaa <rieethaa@gmail.com>
2011-11-04 01:55:35 +0000

    Added code to try model binding

--- public/recipes.js
+++ public/recipes.js
@@ -1,13 +1,16 @@
-window.Recipe = Backbone.Model.extend({
-	
-});
+window.Recipe = Backbone.Model.extend({});
 
 window.RecipeView = Backbone.View.extend({
-	initialize: function(){
+	//el: $('#recipeList')[0],
+	//tagName: 'li',
+	//className: 'recipe',
+	initialize: function() {
+		_.bindAll(this, 'render');
+		this.model.bind('change', this.render);
 		this.template = _.template($('#recipeTemplate').html());
 	},
 	
-	render: function(){
+	render: function() {
 		var renderedContent = this.template(this.model.toJSON());
 		$(this.el).html(renderedContent);
 		return this;
