991b58b136d232a95a9444e3bdcd75bd594f6249
Rieethaa <rieethaa@gmail.com>
2011-11-05 13:51:29 +0000

    Rather than creating a new el and giving a tagname, get the id of the el i.e #recipeList and render the html in that id. Also added some code to temporarily automate the page rendering

--- public/recipes.js
+++ public/recipes.js
@@ -1,7 +1,7 @@
 window.Recipe = Backbone.Model.extend({});
 
 window.RecipeView = Backbone.View.extend({
-	//el: $('#recipeList')[0],
+	el: '#recipeList',
 	//tagName: 'li',
 	//className: 'recipe',
 	initialize: function() {
@@ -15,4 +15,10 @@ window.RecipeView = Backbone.View.extend({
 		$(this.el).html(renderedContent);
 		return this;
 	}
+});
+
+$(function(){
+	var recipe = new Recipe({name:'Shepu chi Bhaji', chef:'Rieethaa'});
+	var recipeView = new RecipeView({model:recipe});
+	recipeView.render();
 });
