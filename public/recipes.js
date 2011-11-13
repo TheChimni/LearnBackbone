9528b9f0717bd70b38f2ee7b058217fe59d8f443
Rieethaa <rieethaa@gmail.com>
2011-11-13 12:43:02 +0000

    Refactored the view to create a new li inside the template to represent recipe summary

--- public/recipes.js
+++ public/recipes.js
@@ -1,24 +1,30 @@
 window.Recipe = Backbone.Model.extend({});
 
-window.RecipeView = Backbone.View.extend({
-	el: '#recipeList',
-	//tagName: 'li',
-	//className: 'recipe',
+window.RecipeSummaryView = Backbone.View.extend({
+	//el: '#recipeList',
+	tagName: 'li',
+	className: 'recipe',
 	initialize: function() {
 		_.bindAll(this, 'render');
 		this.model.bind('change', this.render);
-		this.template = _.template($('#recipeTemplate').html());
+		this.template = _.template($('#recipeSummaryTemplate').html());
 	},
 	
 	render: function() {
 		var renderedContent = this.template(this.model.toJSON());
 		$(this.el).html(renderedContent);
 		return this;
-	}
+	}		
 });
 
 $(function(){
 	var recipe = new Recipe({name:'Shepu chi Bhaji', chef:'Rieethaa'});
-	var recipeView = new RecipeView({model:recipe});
+	var recipeView = new RecipeSummaryView({model:recipe});
 	recipeView.render();
+	$('#recipeList').append(recipeView.el);
+	
+	var recipeAnother = new Recipe({name:'Bangan Ka Bharta', chef:'Stevey'});
+	var recipeAnotherView = new RecipeSummaryView({model:recipeAnother});
+	recipeAnotherView.render();
+	$('#recipeList').append(recipeAnotherView.el); 
 });
