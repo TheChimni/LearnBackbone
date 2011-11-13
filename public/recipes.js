1fbac44f6820db97291a0d2ab5cafa4ae111c229
Rieethaa <rieethaa@gmail.com>
2011-11-13 19:32:23 +0000

    Implemented the event handler to render recipe ingredients and steps

--- public/recipes.js
+++ public/recipes.js
@@ -20,11 +20,30 @@ window.RecipeSummaryView = Backbone.View.extend({
 		return this;
 	},
 	
-	toggleDetails: function(){		
-		console.log('toggle on');
+	toggleDetails: function(){
+		// we need to tell the view which element it maps to using a scoped selector because in the whole
+		// document there will be more than 1 'details' divs. Hence we need to pass el as a parameter to the 
+		// constructor function 
+		this.detailsView = new RecipeDetailView({el: this.$('.details')[0], model: this.model});
+		this.detailsView.render();
 	}
 });
 
+
+window.RecipeDetailView = Backbone.View.extend({
+	
+	initialize: function(){
+		_.bindAll(this, 'render');
+		this.model.bind('change', this.render);
+		this.template = _.template($('#recipeDetailTemplate').html());
+	},
+	
+	render: function(){
+		var renderedContent =  this.template(this.model.toJSON());
+		$(this.el).html(renderedContent);
+		return this;
+	}
+})
 
 window.Recipes = Backbone.Collection.extend({
 	model : Recipe
@@ -50,11 +69,11 @@ window.RecipeListView = Backbone.View.extend({
 
 $(function(){
 	
-	var recipe = new Recipe({name:'Shepu chi Bhaji', chef:'Rieethaa'});
+	var recipe = new Recipe({name:'Shepu chi Bhaji', chef:'Rieethaa', ingredients: 'Dill, garlic, green chillies', steps: 'secret'});
 	
-	var recipeAnother = new Recipe({name:'Baingan Ka Bharta', chef:'Stevey'});
+	var recipeAnother = new Recipe({name:'Baingan Ka Bharta', chef:'Stevey', ingredients: 'garlic with kernels removed', steps: 'secret'});
 	
 	var recipes =  new Recipes();
 	var recipeListView = new RecipeListView({collection: recipes});
-	recipes.reset([recipe, recipeAnother]);	
+	recipes.reset([recipe, recipeAnother]);
 });
